b017345f31f7bf558fa1216ab089cf48a4ef073f
pecorium <pecorium@gmail.com>
2019-05-01 17:14:36 +0900

    Add README.md

--- list2.cpp
+++ list2.cpp
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("printfによる出力\n");
-    puts("putsによる出力");
-    puts("putsによる出力は\\nを付加しなくても改行されます");
-    return 0;
+int main(void) {
+  printf("printfによる出力\n");
+  puts("putsによる出力");
+  puts("putsによる出力は\\nを付加しなくても改行されます");
+  return 0;
 }
